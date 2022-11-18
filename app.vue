da5d394f077b6850326dfccba35ae6a66fed8bc9
Alois Sečkár <ellrohir@seznam.cz>
2022-11-18 18:07:38 +0100

    all css to tailwind

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div>
     <TheHeader />
     <TheMenu />
-    <h1 class="ml-32 mb-2 font-sans font-bold text-4xl text-navy">{{ title }}</h1>
+    <h1 class="ml-32 mb-2 font-bold text-4xl text-navy">{{ title }}</h1>
     <div class="mx-32 mb-12">
       <NuxtPage />
     </div>
@@ -28,22 +28,3 @@ useHead({
 useNewsStore().fill()
 
 </script>
-
-<style>
-#main {
-  margin: 0px;
-  background-color: #faebd7;
-  font-family: "Helvetica Neue";
-  font-size: 14;
-  text-align: justify;
-}
-
-#header {
-  width: 100%;
-  height: 120px;
-  background-image: url('/header.png');
-  background-position: center;
-  background-repeat: repeat-x;
-  background-size: auto 120px;
-}
-</style>
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,47 +1,17 @@
 <template>
-    <div>
-        <client-only>
-            <div id="footer">
-                © {{ year }} <strong>{{ title }} </strong> by <a v-show="website" :href="website">{{ author }}</a><span v-show="!website">>{{ author }}</span>
-                powered by <a href="https://v3.nuxtjs.org/">Nuxt3</a>
-                | <span><NuxtLink to="/login"><fa icon="key" /></NuxtLink></span>
-            </div>
-        </client-only>
+    <div class="fixed bottom-0 w-full py-1 bg-navy text-center text-sm text-silver">
+        © {{ year }} <strong>{{ title }}</strong> by 
+        <a v-show="website" :href="website">{{ author }}</a>
+        <span v-show="!website">>{{ author }}</span>
+        powered by <a href="https://v3.nuxtjs.org/">Nuxt3</a>
+        | <span class="text-xs"><NuxtLink to="/login"><fa icon="key" /></NuxtLink></span>
     </div>
 </template>
 
 <script setup lang="ts">
-    const runtimeConfig = useRuntimeConfig()
-    const title = runtimeConfig.public.textTitle
-    const author = runtimeConfig.public.textAuthorName
-    const website = runtimeConfig.public.textAuthorWebsite
-    const year = new Date().getFullYear()
+const runtimeConfig = useRuntimeConfig()
+const title = runtimeConfig.public.textTitle
+const author = runtimeConfig.public.textAuthorName
+const website = runtimeConfig.public.textAuthorWebsite
+const year = new Date().getFullYear()
 </script>
-
-
-<style scoped>
-    #footer {
-        position: fixed;
-        width: 100%;
-        left: 0;
-        bottom: 0;
-        background-color: #333333;
-        color: #cccccc;
-        text-align: center;
-        padding-bottom: 3px;
-        padding-top: 4px;
-    }
-
-    #footer > span {
-        font-size: 10px;
-    }
-    
-    a:link, a:visited {
-        color: #cccccc;
-    }
-
-    a:hover, a:active {
-        color: white;
-    }
-
-  </style>
