898bcde09de1affeeafda73ab6060068f4642dcc
Alois Sečkár <ellrohir@seznam.cz>
2022-12-19 21:16:20 +0100

    login via supabase POC

---
+++ composables/useLoginStore.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+
+export const useLoginStore = defineStore({
+    id: 'login-store',
+    state: () => {
+        return {
+            user: '',
+        }
+    },
+    actions: {
+        async login(email: string, password: string) {
+            const { data, error } = await useSupabaseClient().auth.signInWithPassword({ email, password })
+            console.log(data)
+
+            if (data) {
+                console.log(data)
+                console.log(data.user.email)
+                this.user = data.user.email
+            } else {
+                console.log(error)
+            }
+        },
+        logout() {
+            this.user = ''
+        }
+    },
+    getters: {
+    }
+})
--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,43 @@
 <template>
     <div>
-        Login for admins
+        <div>
+        </div>
+        <div v-if="user">
+            <div :class="header">Přihlášený uživatel:</div>
+            <div>
+                {{ user }}
+            </div>
+            <button :class="button" @click="attemptLogout()">Logout</button>
+        </div>
+        <div v-else>
+            <div :class="header">Přihlásit:</div>
+            <div>
+                <label>E-mail: </label>
+                <input type="text" name="email" v-model="email" />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="password" name="password" v-model="password" />
+            </div>
+            <button :class="button" @click="attemptLogin()">Login</button>
+        </div>
     </div>
 </template>
+
+<script setup lang="ts">
+const header = "text-xl font-bold"
+const button = "p-1 border border-black bg-navy text-silver"
+
+let email = ''
+let password = ''
+
+const user = computed(() => useLoginStore().user)
+
+async function attemptLogin() {
+    useLoginStore().login(email, password)
+}
+async function attemptLogout() {
+    useLoginStore().logout()
+}
+
+</script>
