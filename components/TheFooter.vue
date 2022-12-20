516e0054a198cc2927c288a2ddf657726972aae3
Alois Sečkár <ellrohir@seznam.cz>
2022-12-20 18:38:34 +0100

    user in footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,7 +2,15 @@
     <div class="fixed bottom-0 w-full py-1 bg-navy text-center text-sm text-silver">
         © {{ year }} <strong>{{ title }}</strong> by <span v-html="author"></span>
         powered by <a href="https://nuxt.com/">Nuxt3</a>
-        | <span class="text-xs"><NuxtLink to="/login"><fa icon="key" /></NuxtLink></span>
+        | 
+        <span class="text-xs">
+            <NuxtLink to="/login">
+                <fa icon="key" />
+            </NuxtLink>
+            <div class="inline" v-if="user">
+                &nbsp;{{ user }}
+            </div>
+        </span>
     </div>
 </template>
 
@@ -11,4 +19,5 @@ const title = useRuntimeConfig().public.textTitle
 const author = computed(() => useTextStore().getKey('web-author'))
 console.log(author.value)
 const year = new Date().getFullYear()
+const user = computed(() => useLoginStore().user)
 </script>
