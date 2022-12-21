469f3750f04f2c06c001092cd3dc38e4b33bb0dd
Alois Sečkár <ellrohir@seznam.cz>
2022-12-21 20:28:23 +0100

    refactoring - h1 title into component

--- app.vue
+++ app.vue
@@ -2,14 +2,7 @@
   <div>
     <TheHeader />
     <TheMenu />
-    <h1 class="ml-32 mb-2 font-bold text-4xl text-navy">
-      {{ title }}
-      <div class="inline text-3xl text-green-500 hover:text-yellow-400" v-if="adminLink">
-        <NuxtLink :to="{ path: '/admin/add/' + adminLink.link }">
-          <fa icon="plus" :title="adminLink.text" />
-        </NuxtLink>
-      </div>
-    </h1>
+    <TheTitle />
     <div class="mx-32 mb-12">
       <NuxtPage />
     </div>
@@ -20,10 +13,8 @@
 <script setup lang="ts">
 import './assets/css/tailwind.css'
 
-const runtimeConfig = useRuntimeConfig()
-const title = runtimeConfig.public.textTitle
 useHead({
-  title: title,
+  title: useRuntimeConfig().public.textTitle,
   bodyAttrs: {
     id: 'main',
   },
@@ -46,37 +37,4 @@ await useGalleryStore().fill()
 await useImageStore().fill()
 await useLinkStore().fill()
 
-// TODO make component for displaying title
-const adminLink = computed(() => {
-  if (useLoginStore().user) {
-    if (useRoute().path.includes('article')) {
-      return {
-        link: 'article',
-        text: 'Add article',
-      }
-    } else if (useRoute().path.includes('galler')) {
-      return {
-        link: 'gallery',
-        text: 'Add gallery',
-      }
-    } else if (useRoute().path.includes('image')) {
-      return {
-        link: 'image',
-        text: 'Add image',
-      }
-    } else if (useRoute().path.includes('book')) {
-      return {
-        link: 'book',
-        text: 'Add book',
-      }
-    } else if (useRoute().path.includes('link')) {
-      return {
-        link: 'link',
-        text: 'Add link',
-      }
-    } 
-  } else {
-    return null
-  }
-})
 </script>
---
+++ components/TheTitle.vue
@@ -0,0 +1,47 @@
+<template>
+    <h1 class="ml-32 mb-2 font-bold text-4xl text-navy">
+      {{ title }}
+      <div class="inline text-3xl text-green-500 hover:text-yellow-400" v-if="adminLink">
+        <NuxtLink :to="{ path: '/admin/add/' + adminLink.link }">
+          <fa icon="plus" :title="adminLink.text" />
+        </NuxtLink>
+      </div>
+    </h1>
+</template>
+
+<script setup lang="ts">
+const title = useRuntimeConfig().public.textTitle
+
+const adminLink = computed(() => {
+  if (useLoginStore().user) {
+    if (useRoute().path.includes('article')) {
+      return {
+        link: 'article',
+        text: 'Add article',
+      }
+    } else if (useRoute().path.includes('galler')) {
+      return {
+        link: 'gallery',
+        text: 'Add gallery',
+      }
+    } else if (useRoute().path.includes('image')) {
+      return {
+        link: 'image',
+        text: 'Add image',
+      }
+    } else if (useRoute().path.includes('book')) {
+      return {
+        link: 'book',
+        text: 'Add book',
+      }
+    } else if (useRoute().path.includes('link')) {
+      return {
+        link: 'link',
+        text: 'Add link',
+      }
+    } 
+  } else {
+    return null
+  }
+})
+</script>
