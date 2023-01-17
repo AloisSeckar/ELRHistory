85cf06a868226cbec71461994f45550991aa0b6f
Alois Sečkár <ellrohir@seznam.cz>
2023-01-17 22:11:30 +0100

    admin form as component

---
+++ components/admin/form/Article.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <FormKit type="form" @submit="$emit('save', article)" submit-label="Add">
+            <FormKit
+                type="select" name="author" id="author"
+                label="Author:" validation="required"
+                :options="authors"
+                v-model="article.elrh_author" />
+            <FormKit
+                type="select" name="category" id="category"
+                label="Category:" validation="required"
+                :options="categories"
+                v-model="article.elrh_category" />
+            <FormKit
+                type="text" name="name" id="name"
+                label="Name:" validation="required"
+                v-model="article.name" />
+            <FormKit
+                type="text" name="dscr" id="dscr"
+                label="Description:" validation="required"
+                v-model="article.dscr" />
+            <FormKit
+                type="textarea" name="content" id="content"
+                label="Content:" validation="required"
+                v-model="article.content" />
+            <FormKit
+                type="text" name="thumb" id="thumb"
+                label="Thumb:" validation="required"
+                v-model="article.thumb" />
+            <FormKit
+                type="select" name="gallery" id="gallery"
+                label="Related gallery:"
+                :options="galleries"
+                v-model="article.elrh_gallery" />
+        </FormKit>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { Article } from '@/composables/useArticleStore'
+import { PropType } from 'vue'
+
+defineProps({
+    article: { type: Object as PropType<Article>, required: true },
+})
+defineEmits<{
+  (e: 'save', article: Article): void
+}>()
+
+const authors = useAuthorStore().getItems.map(author => ({ value: author.author_id, label: author.name }))
+const categories = useCategoryStore().getByType('a').map(cat => ({ value: cat.category_id, label: cat.name }))
+const galleries = useGalleryStore().getItems.sort((a, b) => a.name.localeCompare(b.name)).map(gallery => ({ value: gallery.gallery_id, label: gallery.name }))
+</script>
--- pages/admin/add/article.vue
+++ pages/admin/add/article.vue
@@ -1,46 +1,13 @@
 <template>
     <div>
         <h1>Add new article</h1>
-        <FormKit type="form" @submit="save" submit-label="Add">
-            <FormKit
-                type="select" name="author" id="author"
-                label="Author:" validation="required"
-                :options="authors"
-                v-model="article.elrh_author" />
-            <FormKit
-                type="select" name="category" id="category"
-                label="Category:" validation="required"
-                :options="categories"
-                v-model="article.elrh_category" />
-            <FormKit
-                type="text" name="name" id="name"
-                label="Name:" validation="required"
-                v-model="article.name" />
-            <FormKit
-                type="text" name="dscr" id="dscr"
-                label="Description:" validation="required"
-                v-model="article.dscr" />
-            <FormKit
-                type="textarea" name="content" id="content"
-                label="Content:" validation="required"
-                v-model="article.content" />
-            <FormKit
-                type="text" name="thumb" id="thumb"
-                label="Thumb:" validation="required"
-                v-model="article.thumb" />
-            <FormKit
-                type="select" name="gallery" id="gallery"
-                label="Related gallery:"
-                :options="galleries"
-                v-model="article.elrh_gallery" />
-        </FormKit>
+        <AdminFormArticle :article="article" @save="save" />
     </div>
 </template>
 
 <script setup lang="ts">
 const authors = useAuthorStore().getItems.map(author => ({ value: author.author_id, label: author.name }))
 const categories = useCategoryStore().getByType('a').map(cat => ({ value: cat.category_id, label: cat.name }))
-const galleries = useGalleryStore().getItems.sort((a, b) => a.name.localeCompare(b.name)).map(gallery => ({ value: gallery.gallery_id, label: gallery.name }))
 
 const article = reactive(useArticleStore().getEmpty)
 article.author_id = authors[0].value
