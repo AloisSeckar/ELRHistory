554d9f063279320901461394a38802c127eff05e
Alois Sečkár <ellrohir@seznam.cz>
2023-01-19 20:09:22 +0100

    component to dynamically select item to edit

---
+++ components/admin/form/SelectItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <FormKit type="form" @submit="$emit('selectItem', option)" submit-label="Edit">
+            <FormKit
+                type="select" name="selectItem" id="selectItem"
+                :label="label" validation="required"
+                :options="options"
+                v-model="option" />
+        </FormKit>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from "vue"
+import { FormKitOptionsItem } from '@formkit/inputs'
+
+const props = defineProps({
+    label: {type: String, required: true},
+    options: {type: Array as PropType<FormKitOptionsItem[]>, required: true},
+    selected: {type: Object as PropType<FormKitOptionsItem>, required: true},
+})
+defineEmits<{
+  (e: 'selectItem', option: String): void
+}>()
+
+const option = props.selected
+</script>
--- pages/admin/edit/article/[id].vue
+++ pages/admin/edit/article/[id].vue
@@ -1,24 +1,42 @@
 <template>
     <div>
+        <AdminFormSelectItem :label="'Select article:'" :options="articleOptions" :selected="selected" @select-item="changeItem"/>
         Edit article <strong>{{ article.name }}</strong>
         <AdminFormArticle :article="article" @save="save" />
     </div>
 </template>
 
 <script setup lang="ts">
-const id = new Number(useRoute().params.id);
-const storedArticle = useArticleStore().getById(id);
+import type { Article } from '@/composables/useArticleStore'
+import { FormKitOptionsItem } from '@formkit/inputs'
 
-const article = reactive(JSON.parse(JSON.stringify(storedArticle)));
-article.author_id = storedArticle.elrh_author.author_id;
+const id = new Number(useRoute().params.id);
+
+const selected: FormKitOptionsItem = reactive({ value: '', label: '' })
+const articleOptions = [] as FormKitOptionsItem[];
+for (let i = 0; i < useArticleStore().items.length; i++) {
+    const item: Article = useArticleStore().items[i]
+    articleOptions.push({ value: item.article_id, label: item.name })
+    if (item.article_id == id) {
+        selected.value = item.article_id
+        selected.label = item.name
+    }
+}
+
+const currentArticle = useArticleStore().getById(id)
+const article = reactive(JSON.parse(JSON.stringify(currentArticle)));
+article.author_id = currentArticle.elrh_author.author_id;
 delete article.elrh_author;
-article.category_id = storedArticle.elrh_category.category_id;
+article.category_id = currentArticle.elrh_category.category_id;
 delete article.elrh_category;
-article.gallery_id = storedArticle.elrh_gallery?.gallery_id;
+article.gallery_id = currentArticle.elrh_gallery?.gallery_id;
 delete article.elrh_gallery;
 
 const save = () => {
     // TODO category and gallery not changing
     useArticleStore().update(JSON.parse(JSON.stringify(article)));
+}
+const changeItem = (calback: String) => {
+    navigateTo('/admin/edit/article/' + calback)
 }
 </script>
