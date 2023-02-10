625aa334674e7c1ffb1ba061cc1e76cba904fa9e
Alois Sečkár <ellrohir@seznam.cz>
2023-02-10 19:50:12 +0100

    fix eslint error in db types

--- database/types.ts
+++ database/types.ts
@@ -1,41 +1,43 @@
 import type { definitions } from './_api'
 
-export type Category = definitions['elrhCategory'];
-export type Author = definitions['elrhAuthor'];
+export type ArticleRaw = definitions['elrhArticle']
+export type GalleryRaw = definitions['elrhGallery']
 
-export type ArticleRaw = definitions['elrhArticle'];
+export type Category = definitions['elrhCategory']
+export type Author = definitions['elrhAuthor']
+
 export type ArticleDB = Omit<ArticleRaw, 'articleId'>
 export type Article = ArticleRaw & {
-  elrhAuthor?: Pick<Author, 'authorId' | 'name'>;
-  elrhCategory?: Pick<Category, 'categoryId' | 'name'>;
-  elrhGallery?: Pick<Gallery, 'galleryId' | 'name'>;
+  elrhAuthor?: Pick<Author, 'authorId' | 'name'>
+  elrhCategory?: Pick<Category, 'categoryId' | 'name'>
+  elrhGallery?: Pick<GalleryRaw, 'galleryId' | 'name'>
 }
 
 export type Book = definitions['elrhBook'] & {
-  elrhAuthor: Author;
-  elrhCategory: Category;
+  elrhAuthor: Author
+  elrhCategory: Category
 }
 
-export type Contact = definitions['elrhContact'];
+export type Contact = definitions['elrhContact']
 
-export type Gallery = definitions['elrhGallery'] & {
-  elrhArticle?: Article[];
-  elrhAuthor: Author;
-  parentId?: Gallery;
+export type Gallery = GalleryRaw & {
+  elrhArticle?: Article[]
+  elrhAuthor: Author
+  parentId?: Gallery
 }
 
 export type Image = definitions['elrhImage'] & {
-  elrhAuthor: Author;
-  galleryId: Gallery;
+  elrhAuthor: Author
+  galleryId: Gallery
 }
 
 export type Link = definitions['elrhLink'] & {
-  elrhAuthor: Author;
-  elrhCategory: Category;
+  elrhAuthor: Author
+  elrhCategory: Category
 }
 
 export type News = definitions['elrhNews'] & {
-  elrhAuthor: Author;
+  elrhAuthor: Author
 }
 
-export type Text = definitions['elrhText'];
+export type Text = definitions['elrhText']
