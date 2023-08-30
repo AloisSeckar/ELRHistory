562c4345292ed5f72d3ced96396376bbad7bc98d
Alois Sečkár <ellrohir@seznam.cz>
2023-08-30 21:48:39 +0200

    refactoring - pinia stores to separate dir

---
+++ composables/usePiniaStore.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/guide/directory-structure/composables#how-files-are-scanned
+
+export { useArticleStore } from '@/composables/pinia/useArticleStore'
+export { useAuthorStore } from '@/composables/pinia/useAuthorStore'
+export { useBookStore } from '@/composables/pinia/useBookStore'
+export { useCategoryStore } from '@/composables/pinia/useCategoryStore'
+export { useContactStore } from '@/composables/pinia/useContactStore'
+export { useGalleryStore } from '@/composables/pinia/useGalleryStore'
+export { useImageStore } from '@/composables/pinia/useImageStore'
+export { useLinkStore } from '@/composables/pinia/useLinkStore'
+export { useLoginStore } from '@/composables/pinia/useLoginStore'
+export { useModalStore } from '@/composables/pinia/useModalStore'
+export { useNewsStore } from '@/composables/pinia/useNewsStore'
