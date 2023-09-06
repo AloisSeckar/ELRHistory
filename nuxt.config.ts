0916d4ed164dfc6e8f9823678cf71f69c155d900
Alois Sečkár <ellrohir@seznam.cz>
2023-09-06 22:10:02 +0200

    remove html validation module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
   ssr: false,
   modules: [
     '@vueuse/nuxt',
-    // '@nuxtjs/html-validator', // TODO figure out how to use SSR + i18n correctly
     '@nuxtjs/i18n',
     '@nuxtjs/supabase',
     '@nuxtjs/tailwindcss',
