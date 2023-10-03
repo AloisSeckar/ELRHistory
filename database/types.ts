5166b0c7e6a89b6a9e73118241be369f4729f52d
Alois Sečkár <ellrohir@seznam.cz>
2023-10-03 21:11:53 +0200

    feat: extend data types

--- database/types.ts
+++ database/types.ts
@@ -4,6 +4,7 @@ import type { definitions } from './_api'
 export type ArticleRaw = definitions['elrhArticle']
 export type BookRaw = definitions['elrhBook']
 export type GalleryRaw = definitions['elrhGallery']
+export type ImageRaw = definitions['elrhImage']
 export type NewsRaw = definitions['elrhNews']
 export type LinkRaw = definitions['elrhLink']
 
@@ -25,13 +26,15 @@ export type Book = BookRaw & {
 
 export type Contact = definitions['elrhContact']
 
+export type GalleryDB = Omit<GalleryRaw, 'galleryId'>
 export type Gallery = GalleryRaw & {
   elrhArticle?: Article[]
   elrhAuthor: Author
   parentId?: Gallery
 }
 
-export type Image = definitions['elrhImage'] & {
+export type ImageDB = Omit<ImageRaw, 'galleryId'>
+export type Image = ImageRaw & {
   elrhAuthor: Author
   galleryId: Gallery
 }
@@ -47,11 +50,9 @@ export type News = NewsRaw & {
   elrhAuthor: Author
 }
 
-export type Text = definitions['elrhText']
+export type SupabaseStoreData = Article[] | Author[] | Book[] | Category[] | Contact[] | Gallery[] | Image[] | Link[] | News[]
+export type SupabaseUpdateData = ArticleDB | BookDB | GalleryDB | Image | LinkDB | NewsDB
+export type SupabaseItemType = 'article' | 'book' | 'gallery' | 'image' | 'link' | 'news'
 
-export type SupabaseStoreData = Article[] | Author[] | Book[] | Category[] | Contact[] | Gallery[] | Image[] | Link[] | News[] | Text[]
-export type SupabaseUpdateData = ArticleDB | BookDB | LinkDB | NewsDB
-export type SupabaseItemType = 'article' | 'book' | 'link' | 'news'
-
-export type SupabaseStoreClient = SupabaseClient<Article> | SupabaseClient<Book> | SupabaseClient<News> | SupabaseClient<Link>
-export type SupabaseUpdateClient = SupabaseClient<ArticleDB> | SupabaseClient<BookDB> | SupabaseClient<NewsDB> | SupabaseClient<LinkDB>
+export type SupabaseStoreClient = SupabaseClient<Article> | SupabaseClient<Book> | SupabaseClient<Gallery> | SupabaseClient<Image> | SupabaseClient<Link> | SupabaseClient<News>
+export type SupabaseUpdateClient = SupabaseClient<ArticleDB> | SupabaseClient<BookDB> | SupabaseClient<GalleryDB> | SupabaseClient<ImageDB> | SupabaseClient<LinkDB> | SupabaseClient<NewsDB>
