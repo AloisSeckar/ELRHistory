0b1220e0aa522fe125c99af9b5c5cd984ade0338
Alois Sečkár <ellrohir@seznam.cz>
2023-10-11 22:29:46 +0200

    feat: articles page facelift

--- components/item/Article.vue
+++ components/item/Article.vue
@@ -11,20 +11,20 @@
         </span>
       </div>
       <div class="box-content flex">
-        <div class="flex-none pr-2">
+        <div class="flex-none pr-4">
           <NuxtImg preset="thumb" :src="item.thumb" :alt="item.name" :title="item.name" />
         </div>
         <div class="flex-1">
-          <div class="pb-1">
+          <div :class="[detail ? 'pb-2 border-b-2 italic' : 'pb-1']">
             {{ item.dscr }}
           </div>
-          <div v-if="detail" class="pt-1" v-html="item.content" />
+          <div v-if="detail" class="py-2" v-html="item.content" />
           <div v-else class="pt-1">
             [ <NuxtLink :to="{ path: '/article/' + item.articleId }">
               {{ $t('articles.read') }}
             </NuxtLink> ]
           </div>
-          <div v-if="detail && item.elrhGallery" class="pt-1">
+          <div v-if="detail && item.elrhGallery" class="mt-2 pt-1 border-t-2">
             [ <NuxtLink :to="{ path: '/gallery/' + item.elrhGallery.galleryId }">
               {{ $t('articles.gallery') }}
             </NuxtLink> ]
@@ -33,6 +33,11 @@
       </div>
     </div>
     <ItemInvalid v-else item="article" />
+    <div v-if="detail" class="text-center m-[-12px]">
+      <NuxtLink :to="{ path: '/articles' }">
+        {{ $t('articles.back') }}
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
