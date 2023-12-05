43f3e0c6c7196c78ecbc581a9e389119e7d0ecb4
Alois Sečkár <ellrohir@seznam.cz>
2023-12-05 21:18:56 +0100

    feat: timeline height

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="m-4 h-[300px] border border-black">
+    <div class="m-4 h-[60vh] border border-black">
       <InfiniTimeline :data-supplier="supplier" css-text-color="#000033" title-format="date" title-date-format="DD.MM.YYYY" />
     </div>
   </div>
