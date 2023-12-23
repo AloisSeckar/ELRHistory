f6aa1afaf867f359820cf28e5ebcbf53c8a12a2e
Alois Sečkár <ellrohir@seznam.cz>
2023-12-23 21:55:04 +0100

    refactor: admin links component

---
+++ components/admin/ManageLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <NuxtLink :to="link">
+      {{ $t(text) }}
+    </NuxtLink>
+  </li>
+</template>
+
+<script setup lang="ts">
+
+type Action = 'add' | 'edit'
+type Item = 'article' | 'book' | 'gallery' | 'image' | 'link' | 'news' | 'timeline'
+
+const props = defineProps<{
+  action: Action
+  item: Item
+}>()
+
+const link = `/admin/${props.action}/${props.item}` + (props.action === 'edit' ? '/first' : '')
+const text = `admin.${props.action}.${props.item}`
+</script>
--- components/admin/Summary.vue
+++ components/admin/Summary.vue
@@ -9,83 +9,26 @@
       <div>
         <div class="inline-block w-48">
           <ul class="list-disc ml-6 mb-2">
-            <li>
-              <NuxtLink to="/admin/add/article">
-                {{ $t('admin.add.article') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/admin/add/book">
-                {{ $t('admin.add.book') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/admin/add/gallery">
-                {{ $t('admin.add.gallery') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/admin/add/image">
-                {{ $t('admin.add.image') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/admin/add/link">
-                {{ $t('admin.add.link') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/admin/add/news">
-                {{ $t('admin.add.news') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/admin/add/timeline">
-                {{ $t('admin.add.timeline') }}
-              </NuxtLink>
-            </li>
+            <AdminManageLink action="add" item="article" />
+            <AdminManageLink action="add" item="book" />
+            <AdminManageLink action="add" item="gallery" />
+            <AdminManageLink action="add" item="image" />
+            <AdminManageLink action="add" item="link" />
+            <AdminManageLink action="add" item="news" />
+            <AdminManageLink action="add" item="timeline" />
           </ul>
         </div>
         <div class="inline-block w-48">
           <ul class="list-disc ml-6 mb-2">
-            <li>
-              <NuxtLink to="/admin/edit/article/first">
-                {{ $t('admin.edit.article') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/admin/edit/book/first">
-                {{ $t('admin.edit.book') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/admin/edit/gallery/first">
-                {{ $t('admin.edit.gallery') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/admin/edit/image/first">
-                {{ $t('admin.edit.image') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/admin/edit/link/first">
-                {{ $t('admin.edit.link') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/admin/edit/news/first">
-                {{ $t('admin.edit.news') }}
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/admin/edit/timeline/first">
-                {{ $t('admin.edit.timeline') }}
-              </NuxtLink>
-            </li>
+            <AdminManageLink action="edit" item="article" />
+            <AdminManageLink action="edit" item="book" />
+            <AdminManageLink action="edit" item="gallery" />
+            <AdminManageLink action="edit" item="image" />
+            <AdminManageLink action="edit" item="link" />
+            <AdminManageLink action="edit" item="news" />
+            <AdminManageLink action="edit" item="timeline" />
           </ul>
         </div>
-        <div class="inline w-32" />
       </div>
     </div>
     <div v-else>
