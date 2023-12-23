b3584c99a973c2bc18afa001edadc66c165effe4
Alois Sečkár <ellrohir@seznam.cz>
2023-12-23 21:39:14 +0100

    feat: admin links in two columns

--- components/admin/Summary.vue
+++ components/admin/Summary.vue
@@ -8,67 +8,78 @@
         {{ user }}
       </div>
       <div>
-        <div class="inline w-32">
+        <div class="inline-block w-48">
           <ul class="list-disc ml-6 mb-2">
             <li>
               <NuxtLink to="/admin/add/article">
                 {{ $t('admin.add.article') }}
-              </NuxtLink>
-              /
-              <NuxtLink to="/admin/edit/article/first">
-                {{ $t('admin.edit.article') }}
               </NuxtLink>
             </li>
             <li>
               <NuxtLink to="/admin/add/book">
                 {{ $t('admin.add.book') }}
               </NuxtLink>
-              /
-              <NuxtLink to="/admin/edit/book/first">
-                {{ $t('admin.edit.book') }}
-              </NuxtLink>
             </li>
             <li>
               <NuxtLink to="/admin/add/gallery">
                 {{ $t('admin.add.gallery') }}
-              </NuxtLink>
-              /
-              <NuxtLink to="/admin/edit/gallery/first">
-                {{ $t('admin.edit.gallery') }}
               </NuxtLink>
             </li>
             <li>
               <NuxtLink to="/admin/add/image">
                 {{ $t('admin.add.image') }}
               </NuxtLink>
-              /
-              <NuxtLink to="/admin/edit/image/first">
-                {{ $t('admin.edit.image') }}
-              </NuxtLink>
             </li>
             <li>
               <NuxtLink to="/admin/add/link">
                 {{ $t('admin.add.link') }}
-              </NuxtLink>
-              /
-              <NuxtLink to="/admin/edit/link/first">
-                {{ $t('admin.edit.link') }}
               </NuxtLink>
             </li>
             <li>
               <NuxtLink to="/admin/add/news">
                 {{ $t('admin.add.news') }}
               </NuxtLink>
-              /
-              <NuxtLink to="/admin/edit/news/first">
-                {{ $t('admin.edit.news') }}
-              </NuxtLink>
             </li>
             <li>
               <NuxtLink to="/admin/add/timeline">
                 {{ $t('admin.add.timeline') }}
               </NuxtLink>
-              /
+            </li>
+          </ul>
+        </div>
+        <div class="inline-block w-48">
+          <ul class="list-disc ml-6 mb-2">
+            <li>
+              <NuxtLink to="/admin/edit/article/first">
+                {{ $t('admin.edit.article') }}
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/admin/edit/book/first">
+                {{ $t('admin.edit.book') }}
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/admin/edit/gallery/first">
+                {{ $t('admin.edit.gallery') }}
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/admin/edit/image/first">
+                {{ $t('admin.edit.image') }}
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/admin/edit/link/first">
+                {{ $t('admin.edit.link') }}
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/admin/edit/news/first">
+                {{ $t('admin.edit.news') }}
+              </NuxtLink>
+            </li>
+            <li>
               <NuxtLink to="/admin/edit/timeline/first">
                 {{ $t('admin.edit.timeline') }}
               </NuxtLink>
