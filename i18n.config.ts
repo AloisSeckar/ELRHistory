04b4dde5dbd0cdd29c4929d76fb23433b1ab760b
Alois Sečkár <ellrohir@seznam.cz>
2023-12-25 18:52:23 +0100

    feat: support for cs language

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,9 +1,10 @@
+import cs from '@/assets/lang/cs.json'
 import en from '@/assets/lang/en.json'
 
 export default defineI18nConfig(() => ({
   legacy: false,
-  locale: 'en',
-  fallbackLocale: 'en',
-  messages: { en },
+  locale: useAppConfig().lang,
+  fallbackLocale: useAppConfig().lang,
+  messages: { cs, en },
   warnHtmlMessage: false
 }))
