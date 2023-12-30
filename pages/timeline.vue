6ff299a161f04dcf50b6d7db504cef773e21d492
Alois Sečkár <ellrohir@seznam.cz>
2023-12-30 18:47:23 +0100

    fix: timeline formatting

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="m-4 h-[60vh] border border-black">
-      <InfiniTimeline :data-supplier="supplier" css-text-color="#000033" title-format="date" title-date-format="DD.MM.YYYY" :logging="true" />
+      <InfiniTimeline :data-supplier="supplier" css-text-color="#000033" :logging="true" />
     </div>
   </div>
 </template>
