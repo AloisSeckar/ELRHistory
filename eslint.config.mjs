42f7c3c416624b01efb12e06fd6823e6cf0086be
Alois Sečkár <ellrohir@seznam.cz>
2024-09-01 18:27:19 +0200

    refactor: eslint config re-organization

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,12 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
 
+  // what files will be processed
+  files: ['**/*.js', '**/*.ts', '**/*.vue'],
+
   // `rules` section can follow, where you can change default eslint behaviour if needed
   // you can adjust or even turn off some rules if you cannot or don't want to satisfy them
   // it is not recommended to do so though
-  files: ['**/*.js', '**/*.ts', '**/*.vue'],
   rules: {
     // default for this rule is "1", but I find it too restrictive
     // https://eslint.vuejs.org/rules/max-attributes-per-line.html
