fef63eb1b2131ea87ed898501ed6930455b1c41d
Alois Sečkár <ellrohir@seznam.cz>
2024-12-22 20:00:56 +0100

    feat: quick link to add image into gallery

--- components/admin/LinkIcon.vue
+++ components/admin/LinkIcon.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="inline-block px-2">
-    <NuxtLink :to="{ path: '/admin/edit/' + link }">
-      <span :title="useTAdminEdit(item)">
-        <BaseIcon icon="wrench" size="1.125rem" css-class="mb-1.5 text-green-500 hover:text-yellow-400" />
+    <NuxtLink :to="{ path }">
+      <span :title>
+        <BaseIcon :icon size="1.125rem" css-class="mb-1.5 text-green-500 hover:text-yellow-400" />
       </span>
     </NuxtLink>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps({
+import type { SupabaseActionType, SupabaseItemType } from '@/database/types'
+
+const props = defineProps({
+  action: { type: String as PropType<SupabaseActionType>, default: 'edit' },
+  item: { type: String as PropType<SupabaseItemType>, default: 'item' },
   link: { type: String, required: true },
-  item: { type: String, default: 'item' },
 })
+
+const isEdit = props.action === 'edit'
+const path = `/admin/${isEdit ? 'edit' : 'add'}/${props.link}`
+const icon = isEdit ? 'wrench' : 'plus'
+const title = isEdit ? useTAdminEdit(props.item) : useTAdminAdd(props.item)
 </script>
