fbf6ddad66ebec1718f1002857fa7acbe5306540
Alois Sečkár <ellrohir@seznam.cz>
2025-10-25 08:13:20 +0200

    build: bump to nuxt-ignis v0.5.0-rc.2

--- composables/useTranslation.ts
+++ composables/useTranslation.ts
@@ -1,4 +1,4 @@
-import { useT as useIgnisT } from '../node_modules/nuxt-ignis/composables/useTranslation'
+import { useT as useIgnisT } from '../node_modules/nuxt-ignis/app/composables/useTranslation'
 
 export function useT(key: string): string {
   return useIgnisT(key)
